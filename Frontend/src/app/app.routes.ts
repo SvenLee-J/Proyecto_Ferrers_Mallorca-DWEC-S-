a894395598c7f17656bed770bd69fb444e5d784f
SvenLee-J <sjansengorreta@cifpfbmoll.eu>
2026-02-06 17:24:31 +0100

    Fase 1 - SetUp Frotend/Configuracion basica del Routing

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    // Redirecciona la pagina principal a /home. 
+    // sin "full" Cualquier path conduceria a /home y habria un bucle que te llevara a /home.
+    // "pathMatch: 'full' " Indica que debe cumplir exactamente /'' para que te redireccione a /home. 
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+
+];
