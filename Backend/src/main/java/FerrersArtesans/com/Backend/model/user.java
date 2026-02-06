489c10f8ab326a8b618e49b22ffe70b14304035c
SvenLee-J <sjansengorreta@cifpfbmoll.eu>
2026-02-06 17:57:26 +0100

    Fase 1 - Modelo de datos minimo(de momento)

---
+++ Backend/src/main/java/FerrersArtesans/com/Backend/model/user.java
@@ -0,0 +1,41 @@
+package FerrersArtesans.com.Backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity // Indica a Spring que es una entidad.
+@Table (name = "users") // Nombre de la tabla en BD.
+@Data // Genera Getters y Setters.
+@NoArgsConstructor // Nos evita crear Constructores Vacios.
+@AllArgsConstructor // Genera los constructores automaticamente.
+
+public class user {
+   
+    @Id // Indica que este atributo es un id.
+    // Genera un valor de tipo ID.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    // Solo debe haber un mismo nombre y es el campo es obligatorio.
+    @Column(unique = true, nullable = false)
+    private String nom;
+    
+    // Este campo es obligatorio.
+    @Column(nullable = false)
+    private String password;
+    
+    // Este campo es obligatorio.
+    @Column(nullable = false)
+    private String email;
+
+    // Este campo es obligatorio.
+    @Column(nullable = false)
+    private String rol;
+}
