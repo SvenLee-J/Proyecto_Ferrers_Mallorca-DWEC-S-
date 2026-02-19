27c75e1b8327a863087be9144f1c8d18062d2e14
SvenLee-J <sjansengorreta@cifpfbmoll.eu>
2026-02-19 16:36:20 +0100

    Fase 1 - Autenticación JWT ( Backend ) FUNCIONAL.

--- Backend/src/main/java/FerrersArtesans/com/Backend/security/SecurityConfig.java
+++ Backend/src/main/java/FerrersArtesans/com/Backend/security/SecurityConfig.java
@@ -46,10 +46,10 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())  // Desactiva CSRF (JWT no necesita cockies).
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // Sin sesiones (JWT stateless).
             .authorizeHttpRequests(authz -> authz  // Reglas de acceso.
-                .requestMatchers("/auth/**", "/h2-console/**").permitAll()  // Login y H2 libres.
+                .requestMatchers("/auth/register", "/auth/login").permitAll() // Login y H2 libres.
                 .anyRequest().authenticated()  // TODO lo demas REQUIERE token JWT.
             )
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)  // Agrega filtro JWT ANTES del filtro normal.
+            // .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)  // Agrega filtro JWT ANTES del filtro normal.
             .cors(cors -> cors.configurationSource(corsConfigurationSource()));  // Habilita CORS para Angular.
 
         return http.build();
